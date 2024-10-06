6b4b28d3a699aa36756299cf6c434a5422cdc585
Ulrike <ulrikebloch@icloud.com>
2024-10-06 16:44:38 +0200

    Bug Fix

--- src/scripts/uploadText.ts
+++ src/scripts/uploadText.ts
@@ -4,15 +4,23 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 
+import dotenv from 'dotenv';
+dotenv.config();
 
-import wallet from "../wallet.json";
+const walletPrivateKeyString = process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY;
+if (!walletPrivateKeyString) {
+  throw new Error('WALLET_PRIVATE_KEY is not defined in your .env file');
+}
+const walletPrivateKeyArray: number[] = JSON.parse(walletPrivateKeyString);
+const walletPrivateKeyUint8Array = new Uint8Array(walletPrivateKeyArray);
+console.log(walletPrivateKeyUint8Array);
 
 
 
 export const uploadText = async (text: string) => {
 
     const umi = createUmi("https://api.mainnet-beta.solana.com", "finalized")
-    let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+    let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(walletPrivateKeyUint8Array));
     const myKeypairSigner = createSignerFromKeypair(umi, keypair);
     umi.use(signerIdentity(myKeypairSigner)).use(irysUploader());
     
