d383ff62a6c26d2ec7849f0d5e2c6c3edb797263
Ulrike <ulrikebloch@icloud.com>
2024-10-06 22:51:18 +0200

    Added details button

--- src/app/lot-y/page.tsx
+++ src/app/lot-y/page.tsx
@@ -43,6 +43,7 @@ const Loty = () => {
     const [all, setAll] = useState<any[]>([]);
     const [nftAssets, setNftAssets] = useState<NftAsset[]>([]);
     const [collectionAll, setCollectionAll] = useState<Collection | null>(null);
+    const [selectedNft, setSelectedNft] = useState<NftAsset | null>(null);  // To track the selected NFT
 
 
     const wallet = useWallet();
@@ -67,6 +68,21 @@ const Loty = () => {
           fetchCollectionData();
       }
   }, [wallet, umi, collectionAll]);
+
+
+
+  // const nftAssets: NftAsset[] = [/* Your NFT data here */];
+
+      // Handle the click on the "Detail" button
+      const handleShowDetails = (nft: NftAsset) => {
+        setSelectedNft(nft);  // Set the selected NFT
+    };
+
+    // Close details
+    const handleCloseDetails = () => {
+        setSelectedNft(null);  // Reset selected NFT to hide the details
+    };
+
 
   
   const handleFetchAllClick = async () => {
@@ -111,43 +127,6 @@ const Loty = () => {
         console.error('Error fetching assets from collection:', error);
     }
 };
-
-// const handleFetchRoyaltyClick = async () => {
-//   try {
-//     const assetsInCollection = await handleFetchAllClick();  
-    
-//     // Filter assets that have the 'royalties' property set
-//     const fetchedAssetsWithRoyalties = assetsInCollection.filter(asset => asset.royalties?.basisPoints);
-    
-//     // Map over the filtered assets to construct your desired data format
-//     const mappedAssets = fetchedAssetsWithRoyalties.map(asset => {
-//       const imageAttr = asset.attributes?.attributeList?.find(attr => attr.key === 'image');
-//       const nftPic = imageAttr ? imageAttr.value : null;
-//       const datumAttr = asset.attributes?.attributeList?.find(attr => attr.key === 'datum');
-//       const datum = datumAttr ? datumAttr.value : null;
-//       const basisPoints = asset.royalties?.basisPoints ?? null;
-
-      
-//       return {
-//         name: asset.name,
-//         owner: asset.owner,
-//         href: `https://solscan.io/token/${asset.publicKey}?cluster=devnet`,
-//         nftPic,
-//         datum,
-//         royalties: asset.royalties.basisPoints
-//       };
-//     });
-
-//     // Update the state with the filtered assets
-//     setNftAssets(mappedAssets);
-//   } catch (error) {
-//     console.error("Error fetching assets with royalties:", error);
-//     // Optionally show a user-friendly message
-//     toast.error("Failed to fetch NFTs with royalties.");
-//   }
-// };
-
-
 
 
     // for image zoom out
@@ -195,14 +174,7 @@ const Loty = () => {
                                     nftAssets.map((nft, index) => (
                                 <div key={nft.href} className='bg-white-100 rounded-lg p-4 shadow'>
                                   <h2 className='text-md opacity-50 mb-2'>{nft.datum}</h2>
-                                  {/* <a
-                                    href={nft.href}
-                                    target='_blank'
-                                    rel='noopener noreferrer'
-                                    className='text-blue-500 italic hover:text-blue-700 transition-all duration-200 mb-2 block'
-                                  >
-                                    {nft.owner.slice(0, 19)}...
-                                  </a> */}
+
                                   {nft.nftPic ? (
                                     <img
                                       onClick={() => setSelectedImage(nft.nftPic)}
@@ -216,32 +188,21 @@ const Loty = () => {
                                     <p>No image available</p>
                                   )}
 
-<div className="mt-2 flex items-start space-x-1">
-  <button
-    onClick={() => console.log(`Details of ${nft.name}`)}
-    className="bg-white text-gray-600 p-1 text-xs rounded border border-gray-300 hover:bg-gray-100 transition opacitiy-50"
-  >
-    <FontAwesomeIcon icon={faQuestionCircle} className="h-4 w-4 text-gray-500" />
-  </button>
-  <button
-    onClick={() => console.log(`Owner: ${nft.owner}`)}
-    className="bg-white text-gray-600 p-1 text-xs rounded border border-gray-300 hover:bg-gray-100 transition opacitiy-50"
-  >
-    <FontAwesomeIcon icon={faHome} className="h-4 w-4 text-grey-500" />
-  </button>
-</div>
+                          <div className="mt-2 flex items-start space-x-1">
+                            <button
+                                onClick={() => handleShowDetails(nft)}
+                              className="bg-white text-gray-600 p-1 text-xs rounded border border-gray-300 hover:bg-gray-100 transition opacitiy-50"
+                            >
+                              <FontAwesomeIcon icon={faQuestionCircle} className="h-4 w-4 text-gray-500" />
+                            </button>
+                            <button
+                              onClick={() => console.log(`Owner: ${nft.owner}`)}
+                              className="bg-white text-gray-600 p-1 text-xs rounded border border-gray-300 hover:bg-gray-100 transition opacitiy-50"
+                            >
+                              <FontAwesomeIcon icon={faHome} className="h-4 w-4 text-grey-500" />
+                            </button>
+                          </div>
 
-
-
-
-
-
-{/* {nft.basisPoints ? (
-    <p>w/ Royalties: {nft.basisPoints / 100}%</p> // Divide by 100 to convert to percentage
-) : (
-    <p>Soulbound</p>
-)}
- */}
 
                                 </div>
                               ))
@@ -271,6 +232,39 @@ const Loty = () => {
 
                     </div>
                 </div>
+
+                                {/* Show NFT details when an NFT is selected */}
+                                {selectedNft && (
+                    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+                        <div className="bg-white p-4 rounded-lg shadow-md">
+                            <h2 className="text-xl font-semibold mb-4">NFT Details</h2>
+                            <p><strong>Date:</strong>{selectedNft.name}</p>
+                            <p><strong>Owner:</strong> {selectedNft.owner}</p>
+
+                            <p><strong>Type:</strong> 
+                              {selectedNft.basisPoints !== null 
+                                ? `${selectedNft.basisPoints / 100}% Royalties`  // Divide by 100 to convert to percentage
+                                : 'Soulbound NFT'}
+                            </p>
+                            <p>
+                            <a 
+                              href={selectedNft.href} 
+                              target="_blank" 
+                              rel="noopener noreferrer" 
+                              className="bg-white transition-all border border-gray-300 duration-200  p-2 mt-4 rounded inline-block text-center hover:bg-gray-100"
+                            >
+                              View on Solscan
+                            </a>
+                            </p>
+                            <button
+                              onClick={handleCloseDetails}
+                              className="bg-gradient-to-r from-green-300 to-blue-300 hover:from-pink-300 hover:to-yellow-200 text-blue-800 transition-all duration-200 border-2 border-transparent text-white p-2 mt-4 rounded"
+                            >
+                              Close
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         </>
     );
